a4d2ed2a1b59dcee5bd1625470bd9ef64fa46604
Sooyong97 <01071236398y@gmail.com>
2025-04-07 18:59:44 +0900

    feat: socket 생성 및 서버 주소 세팅

--- server.c
+++ server.c
@@ -7,5 +7,23 @@
 #define PORT 1111
 
 int main() {
+    int server_fd, client_fd;
+    // 인터넷 주소 구조체 생성
+    struct sockaddr_in server_addr, clinet_addr;
+    socklen_t addr_size;
+    char buffer[1024];
 
+    // socket 생성
+    // AFINET : IPv4 주소 체계 사용
+    // SOCK_STREAM : TCP
+    // 0 : 기본 프로토콜 (TCP/UDP, 현재 TCP)
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // 서버 주소 세팅
+    // IPv4 주소 체계
+    server_addr.sin_family = AF_INET;
+    // 어떤 IP 주소 요청이든 받음
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    // 네트워크 전송용으로 port번호 변환
+    server_addr.sin_port = htons(PORT);
 }
