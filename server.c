a10c56609d16739fd118ae1900498e417ae39e76
Sooyong97 <01071236398y@gmail.com>
2025-04-07 19:45:33 +0900

    feat: 채팅 기능 추가

--- server.c
+++ server.c
@@ -9,8 +9,9 @@
 int main() {
     int server_fd, client_fd;
     // 인터넷 주소 구조체 생성
-    struct sockaddr_in server_addr, clinet_addr;
+    struct sockaddr_in server_addr, client_addr;
     socklen_t addr_size;
+    //문자열 데이터 배열
     char buffer[1024];
 
     // socket 생성
@@ -30,4 +31,46 @@ int main() {
     // bind : 소켓을 주소(IP + PORT)에 연결
     // bind (소켓, 주소 구조체 주소, 구조체 크기)
     bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    // 연결 요청 대기
+    // socket, 대기열 크기
+    listen(server_fd, 1);
+
+    printf("연결 대기 중 \n");
+
+    addr_size = sizeof(client_addr);
+    // accept : client_addr에 클라이언트 IP, port 정보 리턴
+    client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_size);
+    printf("client 연결\n");
+
+    // clinet 정보 출력
+    char *client_ip = inet_ntoa(client_addr.sin_addr);
+    int client_port = ntohs(client_addr.sin_port);
+    printf("접속한 클라이언트 IP : %s, 포트 : %d\n", client_ip, client_port);
+
+    // 채팅 시작
+    while (1) {
+        //새 데이터를 받기 전 데이터 초기화
+        memset(buffer, 0, sizeof(buffer));
+        // 클라이언트의 소켓에서 버퍼로 데이터 저장
+        read(client_fd, buffer, sizeof(buffer));
+        printf("Client: %s\n", buffer);
+
+        // 서버에서 데이터 전송
+        printf("Server: ");
+        // 버퍼 초기화
+        memset(buffer, 0, sizeof(buffer));
+        // 입력을 buffer에 저장
+        // 버퍼의 주소, 최대 읽을 문자 수, 입력 스트림
+        fgets(buffer, sizeof(buffer), stdin);
+        // 데이터 전송
+        // 보낼 대상, 버퍼, 버퍼 길이
+        write(client_fd, buffer, strlen(buffer));
+    }
+    
+    // 소켓 종료
+    close(client_fd);
+    close(server_fd);
+
+    return 0;
 }
